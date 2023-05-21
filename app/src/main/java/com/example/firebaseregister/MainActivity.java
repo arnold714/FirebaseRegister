d8b8f625c35dfb51658677094d8b2f9bb27b0787
1 <arnold714@naver.com>
2023-05-22 00:23:26 +0900

    a

--- app/src/main/java/com/example/firebaseregister/MainActivity.java
+++ app/src/main/java/com/example/firebaseregister/MainActivity.java
@@ -3,41 +3,39 @@ package com.example.firebaseregister;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.firebaseregister.adapters.PostAdapter;
+import com.example.firebaseregister.adapters.ListAdapter;
 import com.example.firebaseregister.models.Post;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private FirebaseAuth mAuth;
+    private FirebaseAuth mAuth= FirebaseAuth.getInstance();
     private DatabaseReference mDatabaseRef = FirebaseDatabase.getInstance().getReference();
-    private RecyclerView mPostRecyclerView;
-    private PostAdapter mAdapter;
-    private List<Post> mDatas;
+    private ListView mListView;
+    ListAdapter listAdapter = null;
+    String uid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mPostRecyclerView = findViewById(R.id.main_recyclerview);
-
-        mDatas = new ArrayList<>();
-        mDatas.add(new Post(null, "title", "contents"));
-        mDatas.add(new Post(null, "title", "contents"));
-        mDatas.add(new Post(null, "title", "contents"));
-
-        mAdapter = new PostAdapter(mDatas);
-        mPostRecyclerView.setAdapter(mAdapter);
-
+        mListView = (ListView) findViewById(R.id.main_listview);
+        listAdapter = new ListAdapter();
+        getBoard();
         findViewById(R.id.main_post_edit).setOnClickListener(this);
     }
 
@@ -49,5 +47,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         startActivity(new Intent(this, PostActivity.class));
+    }
+    public void getBoard() {
+        FirebaseUser firebaseUser = mAuth.getCurrentUser();
+        uid = firebaseUser.getUid();
+        mDatabaseRef.child("post").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Map<String, String> map = (Map) dataSnapshot.getValue();
+                mListView.setAdapter(listAdapter);
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Post post = snapshot.getValue(Post.class);
+                    listAdapter.addItem(post.getTitle(), post.getContents());
+
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/firebaseregister/adapters/ListAdapter.java
@@ -0,0 +1,73 @@
+package com.example.firebaseregister.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.firebaseregister.R;
+import com.example.firebaseregister.models.Post;
+
+import java.util.ArrayList;
+
+public class ListAdapter extends BaseAdapter {
+
+    public ArrayList<Post> postArrayList = null;
+
+    public ListAdapter() {
+        postArrayList = new ArrayList<>();
+    }
+
+    @Override
+    public int getCount() {
+        return postArrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return postArrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final int pos = position;
+        final Context context = parent.getContext();
+
+        if (convertView == null) { // 이때 다른 xml 에서불러오는 view 의 객체값이 존재하지 않는다면 .. 최초 한번 실행
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.item_post, parent, false); // 다른 xml 에 있는 view객체를 가지고 올것임
+        }
+
+        // 불러오는 view 의 객체로 id를 주입함
+        TextView tv_title = (TextView) convertView.findViewById(R.id.item_post_title);
+        TextView tv_contents = (TextView) convertView.findViewById(R.id.item_post_contents);
+
+        Post post = postArrayList.get(position); // 아래 addItem 에서 넣은 값을 다시 board에 넣는 방식을 취함
+
+        // 그 값을 세팅을 함
+        tv_title.setText(post.getTitle());
+        tv_contents.setText(post.getContents());
+
+
+
+        return convertView; //view를 return 함
+    }
+
+    //아이템 데이터 추가를 위한 함수, 개발자가 원하는대로 작성 가능.
+    public void addItem(String title,String contents) {
+        Post item = new Post();
+
+        item.setTitle(title);
+        item.setContents(contents);
+
+        postArrayList.add(item);
+    }
+}
+
